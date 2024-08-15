87e45564763e3a3be37e83289193799dfecfa47d
Pankaj Singh <pankajsingh13042000@gmail.com>
2024-08-16 00:48:00 +0530

    Setup the application Theme in light and dark mode

---
+++ app/src/main/java/com/chatapplication/ui/walkthrough/WalkthroughFragment.kt
@@ -0,0 +1,20 @@
+package com.chatapplication.ui.walkthrough
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chatapplication.R
+
+class WalkthroughFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_walkthrough, container, false)
+    }
+
+}

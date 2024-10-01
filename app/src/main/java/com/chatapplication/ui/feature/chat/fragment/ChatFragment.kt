2324fef6cb5a6f9993d63ec3412aa4958fc8e583
pankaj <pankajsingh.xperge@gmail.com>
2024-10-01 09:39:15 +0530

    Implementing chat edit text field functionality

--- app/src/main/java/com/chatapplication/ui/feature/chat/fragment/ChatFragment.kt
+++ app/src/main/java/com/chatapplication/ui/feature/chat/fragment/ChatFragment.kt
@@ -5,15 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.chatapplication.R
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.chatapplication.databinding.FragmentChatBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.imageview.ShapeableImageView
 
 class ChatFragment : Fragment() {
-
+    private lateinit var binding: FragmentChatBinding
+    private lateinit var btnBack: ImageView
+    private lateinit var profileImage: ShapeableImageView
+    private lateinit var name: TextView
+    private lateinit var videoCall: ImageView
+    private lateinit var call: ImageView
+    private lateinit var menu : ImageView
+    private lateinit var rvMessage: RecyclerView
+    private lateinit var ibEmoji: ImageButton
+    private lateinit var ibAttach: ImageButton
+    private lateinit var ibCamera: ImageButton
+    private lateinit var fabMic: FloatingActionButton
+    private lateinit var fabSend: FloatingActionButton
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+    ): View {
+        binding = FragmentChatBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
